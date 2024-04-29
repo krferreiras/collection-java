82d2151347d162c9697c9b1b27ba4f731316bef4
krferreiras <dossantos.krf@gmail.com>
2024-04-29 00:15:19 -0300

    mudando jdk e estudando lista

--- lista-tarefas-estudo-de-list/src/main/java/ListaTarefa.java
+++ lista-tarefas-estudo-de-list/src/main/java/ListaTarefa.java
@@ -30,4 +30,22 @@ public class ListaTarefa {
     public void obterDescricoesTarefas(){
         System.out.println(tarefalist);
     }
+
+    public static void main(String[] args) {
+        ListaTarefa listaTarefa = new ListaTarefa();
+        System.out.println("O numero total de elementos na lista é: " + listaTarefa.obterNumeroTotalDeTarefas());
+
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        listaTarefa.adicionarTarefa("Tarefa 2");
+        listaTarefa.adicionarTarefa("Tarefa 2");
+
+        System.out.println("O numero total de elementos na lista é: " + listaTarefa.obterNumeroTotalDeTarefas());
+
+        listaTarefa.removerTarefa("Tarefa 2");
+
+        System.out.println("O numero total de elementos na lista é: " + listaTarefa.obterNumeroTotalDeTarefas());
+
+        listaTarefa.obterDescricoesTarefas();
+    }
+
 }
--- lista-tarefas-estudo-de-list/src/main/java/Tarefa.java
+++ lista-tarefas-estudo-de-list/src/main/java/Tarefa.java
@@ -9,4 +9,9 @@ public class Tarefa {
     public String getDescricao() {
         return descricao;
     }
+
+    @Override
+    public String toString() {
+        return descricao;
+    }
 }
